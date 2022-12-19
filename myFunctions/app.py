b28ac4ec30da296c2a2faa78fb5df11d2df07370
user-at-704207050840 <caolenguyen@deltavn.vn>
2022-12-19 14:29:26 +0700

    first

---
+++ myFunctions/app.py
@@ -0,0 +1,10 @@
+import json
+import boto3
+from io import BytesIO
+# from wkhtmltopdf import WKHtmlToPdf
+
+
+def index(event, context):
+    return {
+        'body': json.dumps("test PDF")
+    }
