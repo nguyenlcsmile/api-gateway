fdf7f8ea2b249544d5e026a9615b888e1da6a393
user-at-704207050840 <caolenguyen@deltavn.vn>
2022-12-19 17:30:53 +0700

    first

--- myFunctions/app.py
+++ myFunctions/app.py
@@ -1,26 +1,37 @@
 import json
 import boto3
+import pandas as pd
 from io import BytesIO
-# from wkhtmltopdf import WKHtmlToPdf
 
 
 def index(event, context):
     client = boto3.client('dynamodb')
-    res = client.put_item(
-        TableName='my-table',
-        Item={
-            'id': {
-                'S': '1'
-            },
-            'username': {
-                'S': 'Nguyen Van A'
-            },
-            'email': {
-                'S': 'vana@gmail.com'
+    df = pd.read_csv('data/data.csv').astype(str)
+    listUsers = df.values.tolist()
+    for user in listUsers:
+        res = client.put_item(
+            TableName='my-table',
+            Item={
+                'id': {
+                    'S': user[0]
+                },
+                'username': {
+                    'S': user[1]
+                },
+                'email': {
+                    'S': user[2]
+                },
+                'gender': {
+                    'S': user[3]
+                },
+                'country': {
+                    'S': user[4]
+                },
+                'phone': {
+                    'S': user[5]
+                }
             }
-        }
-    )
-    print(res)
+        )
     return {
         'body': json.dumps(event)
     }
