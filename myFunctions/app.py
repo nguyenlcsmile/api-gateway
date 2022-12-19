363157adcc2c1aba4d2a24c205c3c70db15f2e61
user-at-704207050840 <caolenguyen@deltavn.vn>
2022-12-19 18:13:44 +0700

    first

--- myFunctions/app.py
+++ myFunctions/app.py
@@ -13,37 +13,35 @@ def index(event, context):
         Key='data.csv'
     )
     data = dataCsv.get('Body').read()
-    listData = data.decode('utf-8')
-    print(listData)
-    # df = pd.read_csv('data/data.csv').astype(str)
-    # listUsers = df.values.tolist()
-    # for user in listUsers:
-    #     res = clientdb.put_item(
-    #         TableName='my-table',
-    #         Item={
-    #             'id': {
-    #                 'S': user[0]
-    #             },
-    #             'username': {
-    #                 'S': user[1]
-    #             },
-    #             'email': {
-    #                 'S': user[2]
-    #             },
-    #             'gender': {
-    #                 'S': user[3]
-    #             },
-    #             'country': {
-    #                 'S': user[4]
-    #             },
-    #             'password': {
-    #                 'S': user[5]
-    #             },
-    #             'phone': {
-    #                 'S': user[6]
-    #             }
-    #         }
-    #     )
+    listUsers = data.decode('utf-8')
+
+    for user in listUsers:
+        res = clientdb.put_item(
+            TableName='my-table',
+            Item={
+                'id': {
+                    'S': user[0]
+                },
+                'username': {
+                    'S': user[1]
+                },
+                'email': {
+                    'S': user[2]
+                },
+                'gender': {
+                    'S': user[3]
+                },
+                'country': {
+                    'S': user[4]
+                },
+                'password': {
+                    'S': user[5]
+                },
+                'phone': {
+                    'S': user[6]
+                }
+            }
+        )
     return {
         'body': json.dumps(event)
     }
