2408ce1fa22e82cf704c5da867959412cacfa5cd
user-at-704207050840 <caolenguyen@deltavn.vn>
2022-12-19 15:50:05 +0700

    dev

--- myFunctions/app.py
+++ myFunctions/app.py
@@ -5,6 +5,16 @@ from io import BytesIO
 
 
 def index(event, context):
+    client = boto3.client('dynamodb')
+    res = client.put_item(
+        TableName='my-table',
+        Item={
+            'id': '1',
+            'username': 'Nguyen Van A',
+            'email': 'vana@gmail.com'
+        }
+    )
+    print(res)
     return {
         'body': json.dumps(event)
     }
