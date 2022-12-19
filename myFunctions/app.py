2c8affe64c8f7508b7db4eb45b8f69a54a821220
user-at-704207050840 <caolenguyen@deltavn.vn>
2022-12-19 18:00:03 +0700

    first

--- myFunctions/app.py
+++ myFunctions/app.py
@@ -5,36 +5,43 @@ from io import BytesIO
 
 
 def index(event, context):
-    client = boto3.client('dynamodb')
-    df = pd.read_csv('data/data.csv').astype(str)
-    listUsers = df.values.tolist()
-    for user in listUsers:
-        res = client.put_item(
-            TableName='my-table',
-            Item={
-                'id': {
-                    'S': user[0]
-                },
-                'username': {
-                    'S': user[1]
-                },
-                'email': {
-                    'S': user[2]
-                },
-                'gender': {
-                    'S': user[3]
-                },
-                'country': {
-                    'S': user[4]
-                },
-                'password': {
-                    'S': user[5]
-                },
-                'phone': {
-                    'S': user[6]
-                }
-            }
-        )
+    clientdb = boto3.client('dynamodb')
+    clients3 = boto3.client('s3')
+
+    dataCsv = clients3.get_object(
+        Bucket='customerss',
+        Key='data.csv'
+    )
+    print(dataCsv)
+    # df = pd.read_csv('data/data.csv').astype(str)
+    # listUsers = df.values.tolist()
+    # for user in listUsers:
+    #     res = clientdb.put_item(
+    #         TableName='my-table',
+    #         Item={
+    #             'id': {
+    #                 'S': user[0]
+    #             },
+    #             'username': {
+    #                 'S': user[1]
+    #             },
+    #             'email': {
+    #                 'S': user[2]
+    #             },
+    #             'gender': {
+    #                 'S': user[3]
+    #             },
+    #             'country': {
+    #                 'S': user[4]
+    #             },
+    #             'password': {
+    #                 'S': user[5]
+    #             },
+    #             'phone': {
+    #                 'S': user[6]
+    #             }
+    #         }
+    #     )
     return {
         'body': json.dumps(event)
     }
