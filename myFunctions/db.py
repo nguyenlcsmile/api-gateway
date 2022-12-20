22d1734be39c694546d4516c3c539d3a5c605bac
user-at-704207050840 <caolenguyen@deltavn.vn>
2022-12-20 15:10:18 +0700

    dev

--- myFunctions/db.py
+++ myFunctions/db.py
@@ -16,27 +16,43 @@ class dynamoDB:
                 res = self.client.put_item(
                     TableName=nameTable,
                     Item={
-                        'id': str(item[0]),
-                        'username': str(item[1]),
-                        'email': str(item[2]),
-                        'gender': str(item[3]),
-                        'country': str(item[4]),
-                        'password': str(item[5]),
-                        'phone': str(item[6]),
-                        'image': str(urlImage)
+                        'id': {
+                            'S': item[0]
+                        },
+                        'username': {
+                            'S': item[1]
+                        },
+                        'email': {
+                            'S': item[2]
+                        },
+                        'gender': {
+                            'S': item[3]
+                        },
+                        'country': {
+                            'S': item[4]
+                        },
+                        'password': {
+                            'S': item[5]
+                        },
+                        'phone': {
+                            'S': item[6]
+                        },
+                        'image': {
+                            'S': urlImage
+                        }
                     }
                 )
-
-                return {
-                    'statuscode': 200,
-                    'message': 'Post all items success.'
-                }
 
             except Exception as e:
                 return {
                     'statusCode': 500,
                     'body': 'Something wrong from server!!!'
                 }
+
+        return {
+            'statuscode': 200,
+            'message': 'Post all items success.'
+        }
 
     def getAllItems(self, nameTable):
         res = self.client.scan(TableName=nameTable)
